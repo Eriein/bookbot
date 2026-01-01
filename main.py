ea76a7e0c847d660d71ef2d44a2c4b10670a8761
Eriein <gonzalezerick466@gmail.com>
2026-01-01 13:07:23 -0500

    added count character function to sstats

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import count_number_of_words, get_book_text
+from stats import count_number_of_words, get_book_text, count_characters
 
 def main():
     text = get_book_text("books/frankenstein.txt")
+
     num_words = count_number_of_words(text)
-    print(f"Found {num_words} total words")
+    
+
+    num_chars = count_characters(text)
+    print(num_chars)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@ def get_book_text(book_path):
 def count_number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    chars_map = {}
+    lowered_text = text.lower()
+    for ch in lowered_text:
+        if ch not in chars_map:
+            chars_map[ch] = 1
+        else:
+            chars_map[ch] += 1
+    
+    return chars_map
